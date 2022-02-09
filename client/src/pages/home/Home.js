d39d7d271f3577dbebd2e6633cde1478a23cb812
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-02-08 23:38:19 -0800

    creates sidebars and feed components

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,5 +1,18 @@
+import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./Home.css";
 
 export default function Home() {
-  return <Topbar />;
+  return (
+    <>
+      <Topbar />
+      <div className="home-container">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
+    </>
+  );
 }
