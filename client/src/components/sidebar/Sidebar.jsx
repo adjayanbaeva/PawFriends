46c686422835f27114e523e2a7ed4bee61858bf3
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-02-09 23:08:56 -0800

    adds content to Sidebar component, styles the component

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,53 @@
 import "./Sidebar.css";
+import { RssFeed, Chat, PlayCircle } from "@mui/icons-material";
 
 export default function Sidebar() {
-  return <div className="sidebar">This is a sidebar!</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-wrapper">
+        <ul className="sidebar-list">
+          <li className="sidebar-list-item">
+            <RssFeed className="sidebar-icon" />
+            <span className="sidebar-list-item-text">Feed</span>
+          </li>
+          <li className="sidebar-list-item">
+            <Chat className="sidebar-icon" />
+            <span className="sidebar-list-item-text">Chats</span>
+          </li>
+          <li className="sidebar-list-item">
+            <PlayCircle className="sidebar-icon" />
+            <span className="sidebar-list-item-text">Videos</span>
+          </li>
+        </ul>
+        <button className="sidebar-button">Show more</button>
+        <hr className="sidebar-hr" />
+        <ul className="sidebar-friendlist">
+          <li className="sidebar-friend">
+            <img
+              className="sidebar-friend-img"
+              src="/assets/person/2.jpg"
+              alt=""
+            />
+            <span className="sidebar-friend-name">Jane Doe</span>
+          </li>
+          <li className="sidebar-friend">
+            <img
+              className="sidebar-friend-img"
+              src="/assets/person/3.jpeg"
+              alt=""
+            />
+            <span className="sidebar-friend-name">Selena Gomez</span>
+          </li>
+          <li className="sidebar-friend">
+            <img
+              className="sidebar-friend-img"
+              src="/assets/person/4.jpeg"
+              alt=""
+            />
+            <span className="sidebar-friend-name">Alex Smith</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
