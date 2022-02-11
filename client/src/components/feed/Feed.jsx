054227b7a4a2866f80fb7e46e2cfb0215d1fe003
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-02-11 00:20:32 -0800

    adds dummyDate to store the state, adjusts Post and Feed components to fetch data from dummyData file

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,17 +1,16 @@
 import "./Feed.css";
 import Share from "../share/Share";
 import Post from "../post/Post";
+import { Posts } from "../../dummyData";
 
 export default function Feed() {
   return (
     <div className="feed">
       <div className="feed-wrapper"></div>
       <Share />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {Posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,36 +1,43 @@
 import "./Post.css";
 import { MoreVert, Pets, FavoriteBorder } from "@mui/icons-material";
+import { Users } from "../../dummyData";
 
-export default function Post() {
+export default function Post({ post }) {
+  // console.log(post);
   return (
     <div className="post">
       <div className="post-wrapper">
         <div className="post-top">
           <div className="post-top-left">
             <img
-              src="/assets/person/1.jpeg"
+              src={
+                Users.filter((user) => user.id === post.userId)[0]
+                  .profilePicture
+              }
               alt=""
               className="post-profile-img"
             />
-            <span className="post-user-name">Jane Doe</span>
-            <span className="post-date">5 min ago</span>
+            <span className="post-user-name">
+              {Users.filter((user) => user.id === post.userId)[0].username}
+            </span>
+            <span className="post-date">{post.date}</span>
           </div>
           <div className="post-top-right">
             <MoreVert />
           </div>
         </div>
         <div className="post-center">
-          <span className="post-text">Hey! This is my first post</span>
-          <img className="post-img" src="assets/post/1.jpeg" alt="" />
+          <span className="post-text">{post?.desc}</span>
+          <img className="post-img" src={post.photo} alt="" />
         </div>
         <div className="post-bottom">
           <div className="post-bottom-left">
             <Pets className="like-icon" />
             <FavoriteBorder className="like-icon" />
-            <span className="post-like-counter">32</span>
+            <span className="post-like-counter">{post.like}</span>
           </div>
           <div className="post-bottom-right">
-            <span className="post-comment-text">6 comments</span>
+            <span className="post-comment-text">{post.comment} comments</span>
           </div>
         </div>
       </div>
