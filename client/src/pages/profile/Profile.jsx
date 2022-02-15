4ca87822e1812067597f65845c648274a4ffdf79
Ainur Dzhaianbaeva <adjayanbaeva@gmail.com>
2022-02-14 23:40:00 -0800

    fixes get a user by id endpoint by adding query parameters so that we could fetch data by providing either userid or username

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -3,9 +3,21 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Topbar from "../../components/topbar/Topbar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Profile() {
   const PublicFolder = process.env.REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const res = await axios.get(`/users?username=Mariana Perry`);
+      setUser(res.data);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <>
       <Topbar />
@@ -26,12 +38,12 @@ export default function Profile() {
               />
             </div>
             <div className="profile-info">
-              <h4 className="profile-info-name">User Name</h4>
-              <span className="profile-info-description">Hi all!</span>
+              <h4 className="profile-info-name">{user.username}</h4>
+              <span className="profile-info-description">{user.desc}</span>
             </div>
           </div>
           <div className="profile-right-bottom">
-            <Feed username="Christopher Li" />
+            <Feed username="Mariana Perry" />
             <Rightbar profile />
           </div>
         </div>
